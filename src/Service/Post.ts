0d1f0431cc8cf8b321cf364a06791aa358623d41
edersonff <edersonff_@hotmail.com.br>
2024-11-11 22:23:22 -0300

    feat: post and user services created with insert validation

---
+++ src/Service/Post.ts
@@ -0,0 +1,47 @@
+import { DeepPartial } from "typeorm";
+import { Post } from "../entity/Post";
+import { AppDataSource } from "..";
+
+export class PostService {
+  static async createPost(post: DeepPartial<Post>) {
+    const isInvalid = this.isInvalid(post);
+    if (isInvalid) {
+      return { error: isInvalid.error };
+    }
+
+    AppDataSource.getRepository(Post).save(post);
+  }
+
+  // TODO: Implement body validation, ex: zod or express-validator.
+  private static isInvalid(post: DeepPartial<Post>) {
+    post = this.trim(post);
+
+    const postLength = post.title?.length || 0;
+    const descriptionLength = post.description?.length || 0;
+
+    if (postLength === 0 || postLength > 100) {
+      return {
+        error: "Post is required and must be between 1 and 100 characters",
+      };
+    }
+
+    if (descriptionLength === 0 || descriptionLength > 100) {
+      return {
+        error: "Description is required and must be between 1 and 100 characte",
+      };
+    }
+
+    if (!post.userId) {
+      return { error: "User ID is required" };
+    }
+
+    return false;
+  }
+
+  private static trim(post: DeepPartial<Post>) {
+    post.title = post.title?.trim();
+    post.description = post.description?.trim();
+
+    return post;
+  }
+}
---
+++ src/Service/User.ts
@@ -0,0 +1,57 @@
+import { DeepPartial } from "typeorm";
+import { User } from "../entity/User";
+import { AppDataSource } from "..";
+
+export class UserService {
+  static async createUser(user: DeepPartial<User>) {
+    const isInvalid = this.isInvalid(user);
+    if (isInvalid) {
+      return { error: isInvalid.error };
+    }
+
+    AppDataSource.getRepository(User).save(user);
+  }
+
+  // TODO: Implement body validation, ex: zod or express-validator.
+  private static isInvalid(user: DeepPartial<User>) {
+    user = this.trim(user);
+
+    const fNameLength = user.firstName?.length || 0;
+    const lNameLength = user.lastName?.length || 0;
+    const emailLength = user.email?.length || 0;
+
+    const isEmailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email || "");
+
+    if (fNameLength === 0 || fNameLength > 100) {
+      return {
+        error: "User is required and must be between 1 and 100 characters",
+      };
+    }
+
+    if (lNameLength === 0 || lNameLength > 100) {
+      return {
+        error: "Description is required and must be between 1 and 100 characte",
+      };
+    }
+
+    if (emailLength === 0 || emailLength > 100) {
+      return {
+        error: "Email is required and must be between 1 and 100 characters",
+      };
+    }
+
+    if (!isEmailFormat) {
+      return { error: "Email is not in the correct format" };
+    }
+
+    return false;
+  }
+
+  private static trim(user: DeepPartial<User>) {
+    user.firstName = user.firstName?.trim();
+    user.lastName = user.lastName?.trim();
+    user.email = user.email?.trim();
+
+    return user;
+  }
+}
