07f8211a760b4ad66f27c17591438a9c6e98ce4c
edersonff <edersonff_@hotmail.com.br>
2024-11-11 21:35:10 -0300

    feat: post and user entity created

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
 
-//TODO Crie a entidade de Post
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  title: string;
+
+  @Column({ length: 100 })
+  description: string;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne((type) => User, (user) => user.posts)
+  user: User;
+}
