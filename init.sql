92aae4930d09186d54795517f5aca02473c43fb8
edersonff <edersonff_@hotmail.com.br>
2024-11-11 21:36:03 -0300

    feat: SQL init created

--- init.sql
+++ init.sql
@@ -1,5 +1,16 @@
 USE test_db;
 
---TODO Crie a tabela de user;
+create database user{
+    id int primary key auto_increment,
+    first_name varchar(100) not null,
+    last_name varchar(100) not null,
+    email varchar(100) not null,
+}
 
---TODO Crie a tabela de posts;
+create database post{
+    id int primary key auto_increment,
+    title varchar(100) not null,
+    description varchar(100) not null,
+    user_id int not null,
+    foreign key (user_id) references User(id),
+}
