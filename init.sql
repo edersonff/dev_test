7236ff19dca4ef641de08ecfe1a5634f97570196
edersonff <edersonff_@hotmail.com.br>
2024-11-17 13:19:32 -0300

    fix(sql): init file format

--- init.sql
+++ init.sql
@@ -1,16 +1,16 @@
 USE test_db;
 
-create database user{
+create table user(
     id int primary key auto_increment,
     first_name varchar(100) not null,
     last_name varchar(100) not null,
-    email varchar(100) not null,
-}
+    email varchar(100) not null
+);
 
-create database post{
+create table post(
     id int primary key auto_increment,
     title varchar(100) not null,
     description varchar(100) not null,
     user_id int not null,
-    foreign key (user_id) references User(id),
-}
+    foreign key (user_id) references User(id)
+);
